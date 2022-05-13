ffd688f098cca36d3537e1a1cbcd8a8fe0ad1b55
Clare Budds <clarebudds@yahoo.co.uk>
2022-05-13 18:23:48 +0000

    Refactored code

--- directory.rb
+++ directory.rb
@@ -17,25 +17,33 @@ end
   # return the array of students
 def interactive_menu
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" #9 because we'll be adding more items
-    #2. read the input and save it into a variable    
-    selection = gets.chomp
-    
-    case selection
+    print_menu
+    process(gets.chomp)# 1. print the menu and ask the user what to do
+  end  
+end
+  
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+  
+def process(selection)
+  case selection
     when "1"
-      students = input_students  
+      input_students  
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
-      exit #this  will casue the program to terminate
+      exit 
     else 
       puts "I don't know what you meant, try again"
-    end
   end
 end
 
@@ -44,15 +52,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
+def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
-
 
 interactive_menu
